73e3212d7b3bacdf51fb8fb7272995a62dafe770
Darren10 <darrensmith@daksmith.co.uk>
2026-04-29 16:05:59 +0100

    29th April 2026: Minor fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -26,7 +26,7 @@ export default function AboutPage() {
         </div>
       <p className="text-gray-700 mb-4">
         Tina is a passionate textile designer with a love using colours and painting. 
-        With a background in fashion and textile design, she has a keen eye for detail and a unique approach to creating vibrant and innovative designs. Tina's work is inspired by nature, culture, and the world around her, resulting in captivating patterns and textures that bring life to fabrics.
+        With a background in fashion and textile design, she has a keen eye for detail and a unique approach to creating vibrant and innovative designs. Her work is inspired by nature, culture, and the world around her, resulting in captivating patterns and textures that bring life to fabrics.
       </p>
       <p className="text-gray-700">
         In her free time, Tina enjoys exploring latest trends and spreading her creativity. Feel free to connect with her on her email or check out her projects!
